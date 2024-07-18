9f5e7466d27ea238de5c7abc3a9d74d6e78e9885
Rafael Melo <rafaelnunesmelo@gmail.com>
2024-07-18 11:36:57 -0300

    Teste case para credenciais incorretas

--- mocktotest/mocktotestUITests/mocktotestUITests.swift
+++ mocktotest/mocktotestUITests/mocktotestUITests.swift
@@ -25,9 +25,24 @@ class when_user_clicks_on_login_button: XCTestCase {
         loginButton.tap()
         
         let messageText = app.staticTexts["messageText"]
-        
         XCTAssertEqual(messageText.label, "Required fields are missing")
         
+    }
+    
+    func test_should_display_error_message_for_invalid_credentials() {
+        let usernameTextField = app.textFields["usernameTextField"]
+        usernameTextField.tap()
+        usernameTextField.typeText("JohnDoe")
+        
+        let passwordTextField = app.textFields["passwordTextField"]
+        passwordTextField.tap()
+        passwordTextField.typeText("WrongPassword")
+        
+        let loginButton = app.buttons["loginButton"]
+        loginButton.tap()
+        
+        let messageText = app.staticTexts["messageText"]
+        XCTAssertEqual(messageText.label, "Invalid credentials")
     }
     
     func test_should_navigate_to_dashboard_page_when_authenticated() {
